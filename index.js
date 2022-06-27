36d797c590b03234e3927174e30e752bcb96f641
+ <+>
2022-06-27 17:38:06 +0300

    index.js

--- index.js
+++ index.js
@@ -1,52 +1,36 @@
-//`http://openlibrary.org/search/authors.json?q=$(name)`
-const bookUl = document.getElementById("book-list");
-const bookElemName = document.getElementById("book-name");
-const bookElemImage = document.getElementById("boo");
-const bookElemDescription = document.getElementById("book-description");
-let image = document.querySelector("#boo");
+let books = []
+function getBookData(bookId) {
+  fetch("http://localhost:3000/books/"+ bookId)
+    .then((res) => res.json())
+    .then((bookdata) => {
+      let firstItem = document.querySelector("#book-name");
+      firstItem.innerText = bookdata.name;
+      let description = document.querySelector("#book-description");
+      description.innerText = bookdata.description;
 
-
-document.addEventListener("DOMContentLoaded", () => {
-getBookData();
-  });
-
-  function getBookData() {
-    const url="https://openlibrary.org/api/books?bibkeys=ISBN:9780980200447&jscmd=details&format=json"
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) =>{
-        image.src = data.imageLinks;
-        console.log(image.src)
-        
-      })
-    }
-  
-  
-
- 
-  function bookData(data) {
-    data = JSON.parse(data);
-    data.forEach((data) => {
-      const list = document.createElement("li");
-      list.style.fontSize = "18px";
-      list.innerText = data.name;
-      bookUl.appendChild(list);
-      
-      list.addEventListener("click", () => {
-        bookElemName.textContent = data.name;
-        bookElemName.style.color = "red";       
-        bookElemImage.setAttribute("src", data.image_url);
-        bookElemDescription.textContent = data.description;
-        bookElemDescription.style.fontSize = "18px"
-      });
-    });
-  }
-const bookReviews = document.getElementById('review-list');
-  document.querySelector('#review-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    let text = document.querySelector('#review-form textarea')
-    let listReveiw = document.createElement('li');
-    listReveiw.innerText = text.value;
-    beerReviews.appendChild(listReveiw);
-    document.querySelector('#review-form').reset();
-})
+      let image = document.querySelector("#book-image");
+      image.src = bookdata.image_url;
+    })
+    .catch((error) => console.warn(error));
+}
+getBookData(1)
+function  booksClick(bookId){
+  getBookData(bookId)
+}
+function navDisplay() {
+  fetch("http://localhost:3000/books")
+    .then((res) => res.json())
+    .then((data) => {
+      const ul = document.getElementById("book-list");
+      ul.innerHTML = "";
+      for (const item of data) {
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(item.name));
+        li.id = item.id;
+        li.setAttribute("onclick","bookClick("+item.id+")")
+        ul.appendChild(li);
+      }
+    })
+    .catch((error) => console.warn(error));
+}
+navDisplay();
